23b01e94b6b81a33523e0317a8c642c1df589753
poojareddy3 <poojithavangala@gmail.com>
2022-04-07 12:13:13 -0400

    another try

---
+++ client/src/components/routes/ItemEdit.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from 'axios'
+import Layout from "../shared/Layout";
+import ItemForm from "../shared/ItemForm";
+import apiUrl from '../../apiConfig'
+
+function ItemEdit(){
+    const navigate = useNavigate()
+    const { id } = useParams()  //get the id from the current object to update
+    const [item, setItem] = useState({
+        title: '',
+        link: ''
+    })
+    const [updated, setUpdated] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+       try {
+          //  const response = await axios(`http://localhost:3000/api/items/${id}`)
+           const response = await axios(`${apiUrl}/items/${id}`)
+           console.log(response)
+           setItem(response.data)
+       } catch (error) {
+           console.log(error)
+       }
+      }
+      fetchData()
+    }, [])
+
+    const handleChange = (event) => {
+        //created a placeholder grabbing the value from the user input form
+        const updatedField = { [event.target.name] : event.target.value }
+        //assigned the empty state with the updatedField into one object
+        const editedItem = Object.assign(item, updatedField)
+        //assigned the new object to be updated to the state
+        setItem(editedItem)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+         axios({
+            // url: `http://localhost:3000/api/items/${id}`,
+             url: `${apiUrl}/items/${id}`,
+             method: 'PUT',
+             data: item
+         }).then(() => setUpdated(true)).catch(console.error) 
+    }
+
+    useEffect(() => {
+        if(updated) {
+            return navigate(`/items/${id}`)
+        }
+    })
+
+     return(
+         <Layout>
+             <ItemForm 
+               item={item}
+               handleChange={(e) => handleChange(e)} 
+               handleSubmit={(e) => handleSubmit(e)}
+               cancelPath={`/items/${id}`}
+               />
+         </Layout>
+      )
+    }
+
+export default ItemEdit
